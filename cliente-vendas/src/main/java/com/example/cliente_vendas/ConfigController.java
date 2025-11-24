b4b25827ec9a133dae0d0b9a09ae21e95e593687
HiagoCarlos <hiagocarlos77112233@gmail.com>
2025-11-23 21:08:13 -0300

    att

---
+++ cliente-vendas/src/main/java/com/example/cliente_vendas/ConfigController.java
@@ -0,0 +1,17 @@
+package com.example.cliente_vendas;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${mensagem.boasvindas:Erro ao carregar}")
+    private String mensagem;
+
+    @GetMapping("/mensagem")
+    public String getMensagem() {
+        return this.mensagem;
+    }
+}
